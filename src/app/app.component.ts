728b93a8bd316ec3efb51adce8ea88f501522cb1
Filipe Santos Correa <ifilipe@Filipes-MBP.fritz.box>
2017-02-14 21:27:09 +0100

    Initial commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,69 @@
+/*
+ * Angular 2 decorators and services
+ */
+import {
+  Component,
+  OnInit,
+  ViewEncapsulation
+} from '@angular/core';
+import { AppState } from './app.service';
+
+/*
+ * App Component
+ * Top Level Component
+ */
+@Component({
+  selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: [
+    './app.component.css'
+  ],
+  template: `
+
+    <main>
+      <p>Simple TEST</p>
+
+      <router-outlet></router-outlet>
+    </main>
+  `
+})
+export class AppComponent implements OnInit {
+  public angularclassLogo = 'assets/img/angularclass-avatar.png';
+  public name = 'Angular 2 Webpack Starter';
+  public url = 'https://twitter.com/AngularClass';
+
+  constructor(
+    public appState: AppState
+  ) {}
+
+  public ngOnInit() {
+    console.log('Initial App State', this.appState.state);
+  }
+
+}
+
+/*
+  <nav>
+      <a [routerLink]=" ['./'] "
+        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
+        Index
+      </a>
+      <a [routerLink]=" ['./home'] "
+        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
+        Home
+      </a>
+      <a [routerLink]=" ['./detail'] "
+        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
+        Detail
+      </a>
+      <a [routerLink]=" ['./barrel'] "
+        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
+        Barrel
+      </a>
+      <a [routerLink]=" ['./about'] "
+        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
+        About
+      </a>
+    </nav>
+
+ */
---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home';
+import { DataResolver } from './app.resolver';
+
+export const ROUTES: Routes = [
+  // { path: '',      component: HomeComponent },
+  // { path: 'home',  component: HomeComponent },
+  // { path: 'about', component: AboutComponent },
+  // { path: 'detail', loadChildren: './+detail#DetailModule'},
+  // { path: 'barrel', loadChildren: './+barrel#BarrelModule'},
+  // { path: '**',    component: NoContentComponent },
+];
