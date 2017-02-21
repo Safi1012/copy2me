029164d3c3f35af850c0b982827c61f9023668f8
Filipe Santos Correa <filipe@Filipes-MacBook-Pro-2.local>
2017-02-21 13:58:22 +0100

    Input field of UploadComponent is deselected after pressing the upload button.

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { UploadService } from '../../services/upload/upload.service';
 
 @Component({
@@ -8,6 +8,7 @@ import { UploadService } from '../../services/upload/upload.service';
   providers: [UploadService]
 })
 export class UploadComponent {
+  @ViewChild('linkInput') public linkInput: ElementRef;
 
   public inputFieldText = '';
   public isIOS = (navigator.userAgent.match(/(iPad|iPhone|iPod)/g) &&
@@ -21,6 +22,7 @@ export class UploadComponent {
     if (this.inputFieldText.length > 0) {
       this.uploadService.pushLinkToFirebase(this.inputFieldText);
       this.inputFieldText = '';
+      (this.linkInput.nativeElement as HTMLInputElement).blur();
     }
   }
 }
